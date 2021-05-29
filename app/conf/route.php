c636c746260d0550a9368b30b61c4cec9f1bf155
nillkizz <kotovvsan@yandex.ru>
2021-05-30 01:06:01 +0300

    Removed deprecated routing code.

--- app/conf/route.php
+++ app/conf/route.php
@@ -9,31 +9,6 @@ class Route
 
     public static function buildRoute()
     {
-
-//        $controllerName = "IndexController";
-//        $modelName = "IndexModel";
-//        $viewName = "IndexView";
-//        $action = "index";
-//
-//        $uriParts = explode("?", $_SERVER['REQUEST_URI']);
-//        $route = explode("/", $uriParts[0]);
-//
-//        if ($route[1] != '') {
-//            $controllerName = ucfirst($route[1] . "Controller");
-//            $modelName = ucfirst($route[1] . "Model");
-//            $viewName = ucfirst($route[1] . "View");
-//        }
-//
-//        require_once CONTROLLER_PATH . $controllerName . ".php"; //IndexController.php
-//        require_once MODEL_PATH . $modelName . ".php"; //IndexModel.php
-//        require_once VIEW_PATH . $viewName . ".php"; //IndexModel.php
-//
-//        if (isset($route[2]) && $route[2] != '') {
-//            $action = $route[2];
-//        }
-//
-//        new $controllerName($action);
-
         $app = 'Index';
         $action = "index";
 

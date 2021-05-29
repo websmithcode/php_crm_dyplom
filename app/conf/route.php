daf6073f670d88426a776161a4911349ad0ef399
nillkizz <kotovvsan@yandex.ru>
2021-05-30 01:57:04 +0300

    Added error handling and slashes reducing.

--- app/conf/route.php
+++ app/conf/route.php
@@ -7,30 +7,40 @@ class Route
 
     public static function buildRoute()
     {
+        static::reduceSlashes();
         $app = 'Index';
         $action = "index";
-
         $route = explode("/", Functions::getCurrentPath());
 
         if ($route[1] != '') {
             $app = ucfirst($route[1]);
         }
+        @include_once APPS_PATH . $app . "/Controller.php";
+        @include_once APPS_PATH . $app . "/Model.php";
+        @include_once APPS_PATH . $app . "/View.php";
 
-
-        require_once APPS_PATH . $app . "/Controller.php";
-        require_once APPS_PATH . $app . "/Model.php";
-        require_once APPS_PATH . $app . "/View.php";
-
-        if (isset($route[2]) && $route[2] != '') {
+        if (isset($route[2]) && !empty($route[2])) {
             $action = $route[2];
         }
 
         $controllerName = "\\apps\\$app\\Controller";
+        if ((isset($route[3]) && !empty($route[3])) | !class_exists($controllerName) | !method_exists($controllerName, $action)) {
+            static::errorPage();
+            return;
+        }
         new $controllerName($action);
     }
-
-    public function errorPage()
+    public static function errorPage()
     {
-
+        echo "<h1>404 error</h1><h2>Page not found</h2>";
+    }
+    public static function reduceSlashes(){
+        if (str_contains(Functions::getCurrentPath(), '//')){
+            $reduced = Functions::getCurrentPath();
+            while (str_contains($reduced, '//')){
+                $reduced = preg_replace("/\/{2}/", '/', $reduced);
+            }
+            header("Location: $reduced");
+        }
     }
 }

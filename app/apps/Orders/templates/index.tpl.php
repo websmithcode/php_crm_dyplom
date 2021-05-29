fc6a1d933605b9f694ab0ba81b7a967b9264373f
nillkizz <kotovvsan@yandex.ru>
2021-05-30 00:04:07 +0300

    For info for partner filter in orders - popover replaced by tooltip.

--- app/apps/Orders/templates/index.tpl.php
+++ app/apps/Orders/templates/index.tpl.php
@@ -50,7 +50,7 @@ Functions::includeComponent('DateTimePicker');
                                         </datalist>
                                     </div>
                                     <div class="partner-filter-info input-group-text hover-pointer"
-                                         data-bs-toggle="popover" title="Построение запроса">
+                                         data-bs-toggle="popover">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                                              fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
--- app/assets/js/Orders.js
+++ app/assets/js/Orders.js
@@ -1,6 +1,6 @@
 let popover;
-popover = new bootstrap.Popover(document.querySelector('.partner-filter-info'), {
+popover = new bootstrap.Tooltip(document.querySelector('.partner-filter-info'), {
     container: 'body',
     html: true,
-    content: `\n<div class="d-flex justify-content-between">ID партнера: <code><Число>)</code></div>\n<hr>\n<div class="d-flex justify-content-between">Имя партнера: <code>) <Имя> (</code></div>\n<hr>\n<div class="d-flex justify-content-between">Email: <code>(<почта>)</code></div>\n<hr>\n<div class="d-flex justify-content-between">Реквизиты: <code>[<реквизиты>]</code></div>\n    `
+    title: `<code>"ID&nbsp;партнера") "Имя&nbsp;партнера" ("почта") ["реквизиты"]</code>`
 });

5f045a5e929b450a02df4dc2b675444199c139f9
nillkizz <kotovvsan@yandex.ru>
2021-05-29 18:09:26 +0300

    Changed layout for filters on order page.

--- app/views/templates/content/orders/index.tpl.php
+++ app/views/templates/content/orders/index.tpl.php
@@ -4,73 +4,74 @@ includeComponent('DateTimePickerComponent');
 ?>
 <div class="container">
     <h1>Заказы</h1>
-    <p>
-        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFilters"
+    <div>
+        <div class="d-flex flex-row mb-3">
+
+        <button class="btn btn-primary h-max-content me-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFilters"
                 aria-expanded="false" aria-controls="collapseFilters">
             Фильтры
         </button>
-    </p>
-    <div class="collapse mb-3" id="collapseFilters">
-        <div class="card card-body">
-            <form class="">
-                <div class="row">
-                    <label class="form-label">
-                        Диапазон дат
-                    </label>
+        <div class="collapse w-100" id="collapseFilters">
+            <div class="card card-body">
+                <form class="">
                     <div class="row">
-                        <div class="input-group w-max-content">
-                            <div class="input-group-text">От
-                            </div><?php new DateTimePickerComponent('fromDate', 'fromTime'); ?>
-                        </div>
-                        <div class="input-group w-max-content">
-                            <div class="input-group-text">До
-                            </div><?php new DateTimePickerComponent('toDate', 'toTime'); ?>
+                        <label class="form-label">
+                            Диапазон дат
+                        </label>
+                        <div class="row">
+                            <div class="input-group w-max-content">
+                                <div class="input-group-text">От
+                                </div><?php new DateTimePickerComponent('fromDate', 'fromTime'); ?>
+                            </div>
+                            <div class="input-group w-max-content">
+                                <div class="input-group-text">До
+                                </div><?php new DateTimePickerComponent('toDate', 'toTime'); ?>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <hr>
-                <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                    <a href="<?=getCurrentPath()?>" class="btn btn-danger">Сбросить</a>
-                    <button type="submit" class="btn btn-primary">Применить</button>
-                </div>
-            </form>
+                    <hr>
+                    <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                        <a href="<?= getCurrentPath() ?>" class="btn btn-danger">Сбросить</a>
+                        <button type="submit" class="btn btn-primary">Применить</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-
     <div class="card">
-        <?php if (!empty($pageData['orders'])): ?>
-        <table class="table table-hover m-0">
-            <thead>
-            <tr>
-                <?php
-                foreach (array_keys($pageData['orders'][0]) as $col):
-                    ?>
-                    <th scope="col"><?= $col ?></th>
-                <?php
-                endforeach;
-                ?>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            foreach ($pageData['orders'] as $order):
-                ?>
-                <tr>
-                    <th scope="row"><?= array_shift($order) ?></th>
-                    <?php foreach ($order as $key => $col):
+            <?php if (!empty($pageData['orders'])): ?>
+                <table class="table table-hover m-0">
+                    <thead>
+                    <tr>
+                        <?php
+                        foreach (array_keys($pageData['orders'][0]) as $col):
+                            ?>
+                            <th scope="col"><?= $col ?></th>
+                        <?php
+                        endforeach;
                         ?>
-                        <td><?= $col ?></td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    foreach ($pageData['orders'] as $order):
+                        ?>
+                        <tr>
+                            <th scope="row"><?= array_shift($order) ?></th>
+                            <?php foreach ($order as $key => $col):
+                                ?>
+                                <td><?= $col ?></td>
+                            <?php
+                            endforeach;
+                            ?>
+                        </tr>
                     <?php
                     endforeach;
                     ?>
-                </tr>
-            <?php
-            endforeach;
-            ?>
-            </tbody>
-        </table>
-        <?php else: ?>
-         <p class="m-3">Ничего не найдено</p>
-        <?php endif; ?>
+                    </tbody>
+                </table>
+            <?php else: ?>
+                <p class="m-3">Ничего не найдено</p>
+            <?php endif; ?>
+        </div>
     </div>
-</div>

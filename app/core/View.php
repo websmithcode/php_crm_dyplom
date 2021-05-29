d8a27b649f6d9daa850b7776cf4c0a459620c20c
nillkizz <kotovvsan@yandex.ru>
2021-05-29 06:24:31 +0300

    Added function "includeComponent" to view->render

--- app/core/View.php
+++ app/core/View.php
@@ -3,6 +3,10 @@
 class View {
     public function render($tpl, $pageData) {
 
+        function includeComponent($componentName){
+            include COMPONENT_PATH . $componentName . '/' . $componentName. '.php';
+        }
+
         $cssName =  str_replace('Controller', '', $pageData['controllerName']);
         $pageData['stylesCSS'] = CSS_URI . $cssName . '.css';
         include(TEMPLATE_PATH . "wrapper.php");

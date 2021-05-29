acb851d6b74ef5c2923d1c25a961e64061eb6947
nillkizz <kotovvsan@yandex.ru>
2021-05-29 23:52:18 +0300

    Refactored Orders model and removed "Партнер" column for partners.

--- app/apps/Orders/Model.php
+++ app/apps/Orders/Model.php
@@ -6,55 +6,56 @@ class Model extends \Core\Model
 {
     public function getOrders($filters = null)
     {
+        $_isManager = $_SESSION['user']['LoginRoleID'] == USER_ROLES['MANAGER'];
         $sql = "SELECT 
                         o.OrderID as 'Номер заказа', 
                         DATE_FORMAT(o.OrderDate, '%d.%m.%Y в %H:%i:%s') as 'Дата заказа',
-                        s.StateName as 'Статус',
-                        p.PartnerName as 'Партнер',
-                        CONCAT(c.ClientSurName, ' ', c.ClientName, ' ', c.ClientMiddleName) as 'Клиент',
-                        o.OrderCost as 'Сумма заказа',
+                        s.StateName as 'Статус'," .
+                        ($_isManager ? "p.PartnerName as 'Партнер',
+                        CONCAT(c.ClientSurName, ' ', c.ClientName, ' ', c.ClientMiddleName) as 'Клиент'," : "").
+                        "o.OrderCost as 'Сумма заказа',
                         ROUND(o.OrderCost * p.Commission * 0.01, 2) as 'Комиссия партнера'
                     FROM orders as o 
                     JOIN partners as p on p.PartnerID = o.PartnerID 
                     JOIN states as s on s.StateID = o.StateID
                     JOIN clients as c on c.ClientID = o.ClientID";
         $_where = false;
-        if (!empty($filters)){
-            if (!empty($filters['fromDatetime'])){
+        if (!empty($filters)) {
+            if (!empty($filters['fromDatetime'])) {
                 $sql .= $_where ? " AND" : " WHERE";
                 $strFromDateTime = $filters['fromDatetime']->format('Y-m-d H:i:s');
                 $sql .= sprintf(" o.OrderDate > '%s'", $strFromDateTime);
                 $_where = true;
             }
-            if (!empty($filters['toDatetime'])){
+            if (!empty($filters['toDatetime'])) {
                 $sql .= $_where ? " AND" : " WHERE";
                 $strToDateTime = $filters['toDatetime']->format('Y-m-d H:i:s');
                 $sql .= sprintf(" o.OrderDate < '%s'", $strToDateTime);
                 $_where = true;
             }
-            if (!empty($filters['PartnerID'])){
+            if (!empty($filters['PartnerID'])) {
                 $sql .= $_where ? " AND" : " WHERE";
                 $sql .= sprintf(" p.PartnerID = '%s'", $filters['PartnerID']);
                 $_where = true;
             }
-            if (!empty($filters['PartnerName'])){
+            if (!empty($filters['PartnerName'])) {
                 $sql .= $_where ? " AND" : " WHERE";
                 $sql .= sprintf(" p.PartnerName = '%s'", $filters['PartnerName']);
                 $_where = true;
             }
-            if (!empty($filters['PartnerEmail'])){
+            if (!empty($filters['PartnerEmail'])) {
                 $sql .= $_where ? " AND" : " WHERE";
                 $sql .= sprintf(" p.PartnerEmail = '%s'", $filters['PartnerEmail']);
                 $_where = true;
             }
-            if (!empty($filters['PartnerRequisites'])){
+            if (!empty($filters['PartnerRequisites'])) {
                 $sql .= $_where ? " AND" : " WHERE";
                 $sql .= sprintf(" p.PartnerRequisites = '%s'", $filters['PartnerRequisites']);
                 $_where = true;
             }
         }
 
-        if ($_SESSION['user']['LoginRoleID'] == 2) {
+        if (!$_isManager) {
             $sql .= $_where ? " AND" : " WHERE";
             $sql .= " o.PartnerID = :partnerID";
             $stmt = $this->db->prepare($sql);
@@ -67,7 +68,8 @@ class Model extends \Core\Model
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function getPartners(){
+    public function getPartners()
+    {
         $sql = "SELECT * FROM partners";
         $stmt = $this->db->prepare($sql);
         $stmt->execute();

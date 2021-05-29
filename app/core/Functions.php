ee3ffc5cdb9c701f0a88c5003c09947f0b57b742
nillkizz <kotovvsan@yandex.ru>
2021-05-30 02:01:07 +0300

    Added getAction method to Functions.php

--- app/core/Functions.php
+++ app/core/Functions.php
@@ -11,7 +11,13 @@ class Functions
         return parse_url($_SERVER['REQUEST_URI'])['path'];
     }
 
-    public static function getAppName(){
+    public static function getAppName(): string
+    {
         return ucfirst(explode('/', static::getCurrentPath())[1]);
+    }
+    public static function getActionName(): string
+    {
+        $action = explode('/', static::getCurrentPath())[2];
+        return ucfirst(!empty($action) ? $action : 'Index');
     }
 }
